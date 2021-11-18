12369a6ba72b3f71dbb8e0d6cfa410188bf9d5a5
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-11-18 00:07:16 -0300

    Update one-liner.sh

--- one-liner.sh
+++ one-liner.sh
@@ -1,3 +1,12 @@
+# Procurando sites vulneráveis usando alinedownloader(google dork)
+python3 aline.py -D "site:gov.br ext:asp OR ext:aspx"
+# Range 50
+# output arquivo.txt
+
+# Identificando as url que podem ter o objeto vulnerável
+python3 rabbito-objectdetector.py
+# botando o caminho onde o arquivo.txt foi salvo
+
 # Faz a requesição pegando a parte do request onde contém o __VIEWSTATE
 curl -v -s -k https://EXEMPLO.com/ARQUIVO.aspx | grep VIEW >> arquivo.txt
 
